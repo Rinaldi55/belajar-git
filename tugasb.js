43a3344d3a3357ce1362da7ede64e77d8c091f84
Rinaldi <rinaldi207@gmail.com>
2020-08-28 22:51:08 +0700

    upload tubasb

---
+++ tugasb.js
@@ -0,0 +1,11 @@
+var nama = "John"
+var peran = ""
+
+if (nama != null ){
+    console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`)
+} else if (peran == ""){
+    console.log("nama ${nama} Peran harus di isi!")
+} else {
+    console.log(`Nama ${nama} dan Peran ${peran}`)
+if (nama)
+}

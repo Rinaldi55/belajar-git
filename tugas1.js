20ff56b9bc2f4eb1c201d43971c57553d8e49123
Rinaldi <rinaldi207@gmail.com>
2020-08-28 22:56:14 +0700

    tugas1

---
+++ tugas1.js
@@ -0,0 +1,45 @@
+var sentence3 = 'wow JavaScript is so cool'; 
+var exampleFirstWord3 = sentence3.substring(0, 3); 
+var examplesecondWord3 = sentence3.substring(4,14);
+var examplethirdWord3= sentence3.substring(15,17);
+var examplefourthWord3= sentence3.substring(18,20);
+var examplefifthWord3= sentence3.substring(21);
+
+
+
+var firstWordLength = exampleFirstWord3.length
+var secondWordLength = examplesecondWord3.length
+var thirdWordLength = examplethirdWord3.length
+var fourthWordLength = examplefourthWord3.length
+var fifthWordLength = examplefifthWord3.length
+console.log('First Word: ' + exampleFirstWord3 + ', with length: ' + firstWordLength); 
+console.log('Second Word: ' + examplesecondWord3+',with length:'+secondWordLength); 
+console.log('Third Word: ' + examplethirdWord3 +' with length: '+thirdWordLength);
+console.log('fourth Word: ' + examplefourthWord3 +' with length:'+fourthWordLength);
+console.log('fifth Word: ' + examplefifthWord3 +' with length:'+fifthWordLength);
+
+
+console.log("====================================================================================")
+console.log("tugasa")
+var sentence = "I am going to be React Native Developer"; 
+
+var exampleFirstWord = sentence[0] ; 
+var exampleSecondWord = sentence[2] + sentence[3]  ; 
+var examplethirdWord = sentence[4]+ sentence [5]+ sentence[6] + sentence[7] + sentence[8] + sentence[9];
+var examplefourthWord= sentence [11]+ sentence[12] ;
+var examplefifthWord= sentence[14]+ sentence[15];
+var examplesixthWord= sentence[17]+ sentence[18]+ sentence[19]+sentence[20]+sentence[21];
+var exampleseventhWord= sentence[23]+sentence[24]+sentence[25]+sentence[26]+sentence[27]+sentence[28];
+var exampleeighthWord= sentence[30]+sentence[31]+sentence[32]+sentence[33]+sentence[34]+sentence[35]+sentence[36]+sentence[37]+sentence[38];
+
+console.log('First Word: ' + exampleFirstWord); 
+console.log('Second Word: ' + exampleSecondWord); 
+console.log('Third Word: ' + examplethirdWord); 
+console.log('Fourth Word: ' + examplefourthWord);
+console.log('Fifth Word: ' + examplefifthWord);
+console.log('Sixth Word: ' + examplesixthWord);
+console.log('Seventh Word: ' + exampleseventhWord); 
+console.log('Eighth Word: ' + exampleeighthWord)
+
+console.log("============================================")
+console.log('tugas2')   
